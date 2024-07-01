e0033559afaf3ac78b055a399791c10a64617b14
saraxsin <saraxsin@gmail.com>
2024-07-01 13:52:14 -0600

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,20 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Kiki's is cool, ${myName}`;
+    }
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Kiki's is cool, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
